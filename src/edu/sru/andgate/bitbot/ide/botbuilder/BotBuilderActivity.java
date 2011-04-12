80072f5effa38ce674e6ea85c49b89483ea2ec42
Josh <ciph3rzer0@gmail.com>
2011-04-12 15:19:04 -0400

    Hacked together attaching of the interpreter to the graphics

--- src/edu/sru/andgate/bitbot/ide/botbuilder/BotBuilderActivity.java
+++ src/edu/sru/andgate/bitbot/ide/botbuilder/BotBuilderActivity.java
@@ -1,11 +1,17 @@
 package edu.sru.andgate.bitbot.ide.botbuilder;
 
+import edu.sru.andgate.bitbot.Bot;
 import edu.sru.andgate.bitbot.R;
+import edu.sru.andgate.bitbot.graphics.GameActivity;
+import edu.sru.andgate.bitbot.interpreter.SourceCode;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class BotBuilderActivity extends Activity
 {
+	private Bot _currentBot;
 	
 	/** Called when the activity is first created. */
 	@Override
@@ -24,6 +30,27 @@ public class BotBuilderActivity extends Activity
 		t.setTitle("Basic Turret");
 		t.setSummary("A turret for shooting stuff.");
 		t.setPicID(R.drawable.spinnerturret);
+		
+	}
+	
+	/**
+	 * Sets the code associated with the bot
+	 * @param sc a SourceCode object.
+	 */
+	public void setProgram(SourceCode sc)
+	{
+		if (_currentBot != null )
+			_currentBot.attachSourceCode(sc);
+	}
+	
+	/**
+	 * Start the mission
+	 * @param v the view that called this.
+	 */
+	public void begin(View v)
+	{
+		// Start up the game engine
+		startActivity(new Intent(BotBuilderActivity.this, GameActivity.class));
 	}
 	
 }
