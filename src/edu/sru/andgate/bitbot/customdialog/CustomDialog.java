273ebfd53eb89941c9491f8edbf7d40f48b982ba
nludovici20 <nal7301@sru.edu>
2011-04-23 19:59:35 -0400

    Share Button (Email only) (On longpress of filename in code list)

--- src/edu/sru/andgate/bitbot/customdialog/CustomDialog.java
+++ src/edu/sru/andgate/bitbot/customdialog/CustomDialog.java
@@ -1,5 +1,7 @@
 package edu.sru.andgate.bitbot.customdialog;
 
+import java.io.File;
+
 import edu.sru.andgate.bitbot.R;
 import edu.sru.andgate.bitbot.ide.CodeBuilderActivity;
 import edu.sru.andgate.bitbot.tools.FileManager;
@@ -9,6 +11,7 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -41,7 +44,7 @@ public class CustomDialog extends Dialog
         super.onCreate(savedInstanceState);
         setContentView(R.layout.custom_popup);
         final ListView lst=(ListView)findViewById(R.id.myList);
-        String[] list = {"Save As...", "Rename", "Delete", "Back"};
+        String[] list = {"Share", "Save As...", "Rename", "Delete", "Back"};
         
         lst.setAdapter(new ArrayAdapter<String>(context,R.layout.custom_popup_row, list));      
         
@@ -51,7 +54,17 @@ public class CustomDialog extends Dialog
         	public void onItemClick(AdapterView<?> a, View v, int position, long id) {
         		String clicked = ((TextView) v).getText().toString();
         		Log.v("Test", clicked);
-        		if(clicked.equalsIgnoreCase("Save As...")){
+        		if(clicked.equalsIgnoreCase("Share")){
+        			Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+					emailIntent.setType("text/txt");
+					emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "A Gift From BitBot");
+					/*
+					 *  For sending as attachment (couldnt figure out - would show up but not send 
+					 *	emailIntent.putExtra(android.content.Intent.EXTRA_STREAM, Uri.parse(path to file));
+					*/
+					emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, FileManager.readTextFileFromDirectory("Code", file));
+					context.startActivity(Intent.createChooser(emailIntent, "Sending mail..."));
+        		}else if(clicked.equalsIgnoreCase("Save As...")){
         			promptSaveAs(activity, file);
         		}else if(clicked.equalsIgnoreCase("Rename")){
         			promptRename(activity, file);
